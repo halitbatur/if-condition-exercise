8b82eb149ecae083069591510eda77e31f124ffa
Halit Batur <halit.batur@akinon.com>
2023-03-16 16:42:27 +0300

    Create the exercise

--- app.js
+++ app.js
@@ -1,23 +1,7 @@
 function checkNumber(num) {
-  if (num >= 10 && num <= 20) {
-    console.log(num);
-  } else if (num < 100) {
-    return num / 2;
-  } else if (num > 20) {
-    console.log(num ** 2);
-  } else if (num >= 100 && num < 200) {
-    return num;
-  } else {
-    return num * 2;
-  }
   // Your code here
 }
 
 function checkStringLength() {
   // Your code here
 }
-
-module.exports = {
-  checkNumber,
-  checkStringLength,
-};
